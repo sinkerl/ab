32c56a9b0d81072c0978ce72d5fd58d1afe84173
wangmf <moonllback@outlook.com>
2025-05-16 15:49:46 +0800

    提交首页修改

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,22 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig( {
+    title: '我的个人博客',
+    description: '记录前端与生活',
+    themeConfig: {
+        logo: '/logo.png', // 放一张你的 logo
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '归档', link: '/archive' },
+            { text: '关于', link: '/about' }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/sinkerl' }
+        ],
+    },
+    head: [
+        // 这里添加 favicon
+        ['link', { rel: 'icon', type: 'image/x-icon', href: '/cat_favicon.ico' }]
+    ]
+})
